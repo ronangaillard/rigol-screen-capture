2a9fc69458c96102f5c4577588a3c516d9e79db8
Ronan Gaillard <ronan.gaillard@live.fr>
2018-10-11 16:32:31 +0200

    First commit

---
+++ rigolcap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$SCOPE_IP" ]; then
+    echo "Need to set SCOPE_IP"
+    exit 1
+fi
+
+echo ":DISPLAY:DATA? ON,OFF,PNG" | nc -w1 $SCOPE_IP 5555 | dd bs=1 skip=11 of=$1.png
+
+open $1.png
