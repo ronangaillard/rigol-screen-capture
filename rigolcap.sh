76c7b386b25f3afe0ab871b095c6a29b48ca8d87
Ronan Gaillard <ronan.gaillard@live.fr>
2018-10-11 16:35:32 +0200

    Check for argument

--- rigolcap.sh
+++ rigolcap.sh
@@ -5,6 +5,11 @@ if [ -z "$SCOPE_IP" ]; then
     exit 1
 fi
 
+if [ -z "$1" ]; then
+    echo "Need to pass filename (without extension) as argument"
+    exit 1
+fi
+
 echo ":DISPLAY:DATA? ON,OFF,PNG" | nc -w1 $SCOPE_IP 5555 | dd bs=1 skip=11 of=$1.png
 
 open $1.png
